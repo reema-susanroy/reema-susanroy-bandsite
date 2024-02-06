24d5bee5603e3a6c2dc34332f6219c47000de6a5
Reema Roy <reemaroy73@gmail.com>
2024-02-05 21:48:09 -0800

    Create script files. Add show items to an array.

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,57 @@
+const showsItems = [
+    {
+        dateLabel: "date",
+        venueLabel: "venue",
+        locationLabel: "location",
+        dateItem: "Mon Sept 09 2024",
+        venueItem: "Ronald Lane",
+        locationItem: "San Francisco, CA"
+    },
+
+    {
+        dateLabel: "date",
+        venueLabel: "venue",
+        locationLabel: "location",
+        dateItem: "Tue Sept 17 2024",
+        venueItem: "Pier 3 East",
+        locationItem: "San Francisco, CA"
+    },
+    {
+        dateLabel: "date",
+        venueLabel: "venue",
+        locationLabel: "location",
+        dateItem: "Sat Oct 12 2024",
+        venueItem: "View Lounge ",
+        locationItem: "San Francisco, CA"
+    },
+    {
+        dateLabel: "date",
+        venueLabel: "venue",
+        locationLabel: "location",
+        dateItem: "Sat Nov 16 2024",
+        venueItem: "Hyatt Agency",
+        locationItem: "San Francisco, CA"
+    },
+    {
+        dateLabel: "date",
+        venueLabel: "venue",
+        locationLabel: "location",
+        dateItem: "Fri Nov 29 2024",
+        venueItem: "Moscow Center",
+        locationItem: "San Francisco, CA"
+    },
+    {
+        dateLabel: "date",
+        venueLabel: "venue",
+        locationLabel: "location",
+        dateItem: "Wed Dec 18 2024",
+        venueItem: "Press Club",
+        locationItem: "San Francisco, CA"
+    }
+];
+
+function createShowItems() {
+    let item = document.querySelector(".shows__layout");
+    console.log(item);
+}
+
