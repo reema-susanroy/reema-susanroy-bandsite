92dba1a870dbab129a6ac1eeef8fc2f7f79e4212
Reema Roy <reemaroy73@gmail.com>
2024-02-06 17:18:05 -0800

    Add shows list using DOM for mobile view.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -5,7 +5,8 @@ const showsItems = [
         locationLabel: "location",
         dateItem: "Mon Sept 09 2024",
         venueItem: "Ronald Lane",
-        locationItem: "San Francisco, CA"
+        locationItem: "San Francisco, CA",
+        buttonItem: "Buy Tickets"
     },
 
     {
@@ -14,7 +15,8 @@ const showsItems = [
         locationLabel: "location",
         dateItem: "Tue Sept 17 2024",
         venueItem: "Pier 3 East",
-        locationItem: "San Francisco, CA"
+        locationItem: "San Francisco, CA",
+        buttonItem: "Buy Tickets"
     },
     {
         dateLabel: "date",
@@ -22,7 +24,8 @@ const showsItems = [
         locationLabel: "location",
         dateItem: "Sat Oct 12 2024",
         venueItem: "View Lounge ",
-        locationItem: "San Francisco, CA"
+        locationItem: "San Francisco, CA",
+        buttonItem: "Buy Tickets"
     },
     {
         dateLabel: "date",
@@ -30,7 +33,8 @@ const showsItems = [
         locationLabel: "location",
         dateItem: "Sat Nov 16 2024",
         venueItem: "Hyatt Agency",
-        locationItem: "San Francisco, CA"
+        locationItem: "San Francisco, CA",
+        buttonItem: "Buy Tickets"
     },
     {
         dateLabel: "date",
@@ -38,7 +42,8 @@ const showsItems = [
         locationLabel: "location",
         dateItem: "Fri Nov 29 2024",
         venueItem: "Moscow Center",
-        locationItem: "San Francisco, CA"
+        locationItem: "San Francisco, CA",
+        buttonItem: "Buy Tickets"
     },
     {
         dateLabel: "date",
@@ -46,12 +51,97 @@ const showsItems = [
         locationLabel: "location",
         dateItem: "Wed Dec 18 2024",
         venueItem: "Press Club",
-        locationItem: "San Francisco, CA"
+        locationItem: "San Francisco, CA",
+        buttonItem: "Buy Tickets"
     }
 ];
 
-function createShowItems() {
-    let item = document.querySelector(".shows__layout");
-    console.log(item);
+const showSection = document.querySelector(".shows__layout");
+
+
+function createShowItems(show) {
+    const showCont = document.createElement("div");
+    showCont.classList.add("show__item");
+
+    const dateCont = document.createElement("div");
+    dateCont.classList.add("show__item--cont")
+    const dateLabel = document.createElement("p");
+    dateLabel.classList.add("show__item--label")
+    dateLabel.textContent = show.dateLabel;
+    const dateContent = document.createElement("p");
+    dateContent.classList.add("show__item--value")
+    dateContent.textContent = show.dateItem;
+
+    const venueCont = document.createElement("div");
+    venueCont.classList.add("show__item--cont")
+    const venueLabel = document.createElement("p");
+    venueLabel.classList.add("show__item--label")
+    venueLabel.textContent =show.venueLabel;
+    const venueContent = document.createElement("p");
+    venueContent.classList.add("show__item--value")
+    venueContent.textContent =show.venueItem;
+
+
+    const locationCont = document.createElement("div");
+    locationCont.classList.add("show__item--cont")
+    const locationLabel = document.createElement("p");
+    locationLabel.classList.add("show__item--label")
+    locationLabel.textContent =show.locationLabel;
+    const locationContent = document.createElement("p");
+    locationContent.classList.add("show__item--value")
+    locationContent.textContent =show.locationItem;
+
+
+    // const buttonCont = document.createElement("button");
+    // buttonCont.classList.add("show__item--button");
+    // buttonCont.textContent = show.buttonItem;
+
+    const buttonCont = document.createElement("p");
+    buttonCont.classList.add("show__item--button");
+    buttonCont.textContent = show.buttonItem;
+
+    /*
+    dateCont.appendChild(dateLabel);
+    dateCont.appendChild(dateContent);
+    venueCont.appendChild(venueLabel);
+    venueCont.appendChild(venueContent);
+    locationCont.appendChild(locationLabel);
+    locationCont.appendChild(locationContent);
+
+    showCont.appendChild(dateCont);
+    showCont.appendChild(venueCont);
+    showCont.appendChild(locationCont);
+    */
+    
+    showCont.appendChild(dateLabel);
+    showCont.appendChild(dateContent);
+    showCont.appendChild(venueLabel);
+    showCont.appendChild(venueContent);
+    showCont.appendChild(locationLabel);
+    showCont.appendChild(locationContent);
+       
+    showCont.appendChild(buttonCont);
+    
+    return showCont;
+    
 }
 
+function renderShow(){
+    showsItems.forEach((show) => {
+        const showData = createShowItems(show);
+        showSection.appendChild(showData);
+
+    });
+
+}
+renderShow();
+
+
+function addTabletLabel(event){
+    const tabletLabels = document.querySelectorAll(".show__item--label");
+    for(let i = 0; i < 3; i++){
+        tabletLabels[i].classList.add("show__item--label-tablet");
+    }
+    console.log(tabletLabels);
+}
+addTabletLabel();
